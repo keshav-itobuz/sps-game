a0bde9bac89ab57a094004217326fbffca6a8a13
keshav-itobuz <keshav@itobuz.com>
2024-02-21 13:15:24 +0530

    completed

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const result = document.querySelector('#result');
 const restartButton = document.querySelector('#restart');
-const playerOne = document.querySelector('#user');
-const playerTwo = document.querySelector('#cpu');
+const userScore = document.querySelector('#user');
+const cpuScore = document.querySelector('#cpu');
 const display = document.querySelector('#output');
 const scoreContainer = document.querySelector('#scores')
 const gameOutput = ['stone', 'paper', 'scissors'];
+
 let count = 1;
-restartButton.style.display = "none";
+restartButton.style.visibility = "hidden";
+
 function displayCard(userInput, programInput) {
     display.innerHTML = "";
     let userCard = document.createElement('img');
@@ -20,68 +22,77 @@ function displayCard(userInput, programInput) {
     display.appendChild(cpuCard);
 }
 
+function colorChanger(scoreBorderColor, userScoreColor, CpuScoreColor) {
+    scoreContainer.style.borderColor = scoreBorderColor;
+    userScore.style.color = userScoreColor;
+    cpuScore.style.color = CpuScoreColor;
+}
+
 function gameLogic(userInput, programInput) {
+
     displayCard(userInput, programInput);
-    restartButton.style.display = "none";
+
     if (userInput === programInput) {
         result.innerText = "Its a tie";
-        scoreContainer.style.borderColor = "gray";
-        playerOne.style.color = "white";
-        playerTwo.style.color = "white";
+        colorChanger("gray", "white", "white");
     }
+
     else if ((userInput === 'stone' && programInput === 'scissors') || (userInput === 'paper' && programInput === 'stone') || (userInput === 'scissors' && programInput === 'paper')) {
         result.innerText = "Yay!!! You Won "
-        playerOne.innerText = Number(playerOne.innerText) + 1;
-        scoreContainer.style.borderColor = "green";
-        playerOne.style.color = "green";
-        playerTwo.style.color = "white";
+        userScore.innerText = Number(userScore.innerText) + 1;
+        colorChanger("green", "green", "white");
     }
+
     else {
         result.innerHTML = "Sorry , You Lost"
-        playerTwo.innerText = Number(playerTwo.innerText) + 1;
-        scoreContainer.style.borderColor = "red";
-        playerOne.style.color = "white";
-        playerTwo.style.color = "red";
+        cpuScore.innerText = Number(cpuScore.innerText) + 1;
+        colorChanger("red", "white", "red");
     }
-    count = playerTwo.innerText > playerOne.innerText ? playerTwo.innerText : playerOne.innerText;
+
+    count = cpuScore.innerText > userScore.innerText ? cpuScore.innerText : userScore.innerText;
+
     if (Number(count) === 3) {
-        if (playerOne.innerText > playerTwo.innerText) {
+
+        if (userScore.innerText > cpuScore.innerText) {
             result.innerText = "Congratulations! You Nailed It";
         }
+
         else {
             result.innerText = "Game Over. Better luck next time ;)";
         }
-        restartButton.style.display = "block";
+        restartButton.style.visibility = "visible";
     }
 }
 
 window.addEventListener('click', (e) => {
     if (e.target.id === 'restart') {
         count = 0;
-        playerOne.innerText = 0;
-        playerTwo.innerText = 0;
-        display.innerHTML = "";
+        userScore.innerText = 0;
+        cpuScore.innerText = 0;
         result.innerText = "Let's start";
-        scoreContainer.style.borderColor = "white";
-        playerOne.style.color = "white";
-        playerTwo.style.color = "white";
-        restartButton.style.display = "none";
+        restartButton.style.visibility = "hidden";
+        colorChanger("white", "white", "white");
+        display.innerHTML = "";
+        display.innerHTML = '<span id="userHand">✊🏻</span> <span id="cpuHand" >✊🏻</span>';
     }
 
     if (count < 3) {
         let randomNumber = Math.round((Math.random() * 10) / 5);
+
         if (e.target.id === 'stone' || e.target.id === 'stoneImage') {
             gameLogic(gameOutput[0], gameOutput[randomNumber]);
         }
+
         else if (e.target.id === 'paper' || e.target.id === 'paperImage') {
             gameLogic(gameOutput[1], gameOutput[randomNumber]);
         }
+
         else if (e.target.id === "scissors" || e.target.id === 'scissorsImage') {
             gameLogic(gameOutput[2], gameOutput[randomNumber]);
         }
+
         else {
             return;
         }
     }
-
 })
